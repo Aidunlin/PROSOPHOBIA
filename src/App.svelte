d6e50c834c94b8dfc3e586e537b7d17865baa07c
Aidan Linerud <aidunlin@gmail.com>
2022-04-28 21:41:15 -0700

    Add Player class, add FPS display

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,34 @@
   class Vector {
     x: number;
     y: number;
-    constructor(x: number, y: number) {
-      this.x = x;
-      this.y = y;
+    constructor(x?: number, y?: number) {
+      this.x = x ?? 0;
+      this.y = y ?? 0;
+    }
+    multiplyBy(by: number) {
+      return new Vector(this.x * by, this.y * by);
     }
     rotateBy(by: number) {
       return new Vector(this.x * Math.cos(by) - this.y * Math.sin(by), this.x * Math.sin(by) + this.y * Math.cos(by));
+    }
+  }
+
+  class Player {
+    position: Vector;
+    direction: Vector;
+    get plane(): Vector {
+      return new Vector(-this.direction.y, this.direction.x);
+    }
+    constructor(x?: number, y?: number, dx?: number, dy?: number) {
+      this.position = new Vector(x, y);
+      this.direction = new Vector(dx, dy);
+    }
+    move(by: Vector) {
+      if (!worldMap[Math.floor(this.position.y)][Math.floor(this.position.x + by.x)]) this.position.x += by.x;
+      if (!worldMap[Math.floor(this.position.y + by.y)][Math.floor(this.position.x)]) this.position.y += by.y;
+    }
+    look(by: number) {
+      this.direction = this.direction.rotateBy(by);
     }
   }
 
@@ -47,21 +69,19 @@
     "pics/mossy.png",
     "pics/wood.png",
     "pics/colorstone.png",
-  ]
+  ];
 
-  const textures = imagePaths.map(path => {
+  const textures = imagePaths.map((path) => {
     let image = new Image();
     image.src = path;
     return image;
-  })
+  });
 
   const textureSize = 64;
   let innerWidth = 0;
   let innerHeight = 0;
 
-  let position = new Vector(11.5, 22);
-  let direction = new Vector(0, -1);
-  let plane = new Vector(1, 0);
+  let player = new Player(11.5, 22, 0, -1);
 
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
@@ -69,6 +89,7 @@
   let time = 0;
   let oldTime = 0;
   let frameTime = 0;
+  let fps = 0;
 
   let inputs = {
     w: false,
@@ -87,19 +108,11 @@
     if (inputs.hasOwnProperty(e.key)) inputs[e.key] = false;
   }
 
-  function move(vel: Vector) {
-    if (!worldMap[Math.floor(position.y)][Math.floor(position.x + vel.x)]) position.x += vel.x;
-    if (!worldMap[Math.floor(position.y + vel.y)][Math.floor(position.x)]) position.y += vel.y;
-  }
-
-  function rotate(vel: number) {
-    direction = direction.rotateBy(vel);
-    plane = plane.rotateBy(vel);
-  }
-
   function init() {
     ctx = canvas.getContext("2d");
     ctx.imageSmoothingEnabled = false;
+    ctx.font = "36px monospace";
+    setInterval(() => (fps = 1 / frameTime), 1000);
     window.requestAnimationFrame(draw);
   }
 
@@ -108,8 +121,6 @@
     ctx.fillRect(0, 0, innerWidth, innerHeight);
     ctx.fillStyle = "#707070";
     ctx.fillRect(0, innerHeight / 2, innerWidth, innerHeight);
-    
-    drawWalls();
 
     oldTime = time;
     time = performance.now();
@@ -118,67 +129,72 @@
     let moveSpeed = frameTime * 4;
     let rotateSpeed = frameTime * 3;
 
-    if (inputs.w) move(new Vector(direction.x * moveSpeed, direction.y * moveSpeed));
-    if (inputs.s) move(new Vector(-direction.x * moveSpeed, -direction.y * moveSpeed));
-    if (inputs.a) move(new Vector(direction.y * moveSpeed, -direction.x * moveSpeed));
-    if (inputs.d) move(new Vector(-direction.y * moveSpeed, direction.x * moveSpeed));
-    if (inputs.ArrowLeft) rotate(-rotateSpeed);
-    if (inputs.ArrowRight) rotate(rotateSpeed);
+    if (inputs.w) player.move(player.direction.multiplyBy(moveSpeed));
+    if (inputs.s) player.move(player.direction.multiplyBy(-moveSpeed));
+    if (inputs.a) player.move(new Vector(player.direction.y, -player.direction.x).multiplyBy(moveSpeed));
+    if (inputs.d) player.move(new Vector(-player.direction.y, player.direction.x).multiplyBy(moveSpeed));
+    if (inputs.ArrowLeft) player.look(-rotateSpeed);
+    if (inputs.ArrowRight) player.look(rotateSpeed);
 
+    drawWalls();
+    drawText();
     window.requestAnimationFrame(draw);
   }
 
   function drawWalls() {
     for (let x = 0; x < innerWidth; x++) {
       let cameraX = (2 * x) / innerWidth - 1;
-      let rayDirection = new Vector(direction.x + plane.x * cameraX, direction.y + plane.y * cameraX);
-      let mapCell = new Vector(Math.floor(position.x), Math.floor(position.y));
-
+      let rayDirection = new Vector(
+        player.direction.x + player.plane.x * cameraX,
+        player.direction.y + player.plane.y * cameraX
+      );
       let deltaStep = new Vector(Math.abs(1 / rayDirection.x), Math.abs(1 / rayDirection.y));
 
-      let firstStep = new Vector(
-        (rayDirection.x < 0 ? position.x - mapCell.x : mapCell.x + 1 - position.x) * deltaStep.x,
-        (rayDirection.y < 0 ? position.y - mapCell.y : mapCell.y + 1 - position.y) * deltaStep.y
+      let mapCell = new Vector(Math.floor(player.position.x), Math.floor(player.position.y));
+      let sideDistance = new Vector(
+        (rayDirection.x < 0 ? player.position.x - mapCell.x : mapCell.x + 1 - player.position.x) * deltaStep.x,
+        (rayDirection.y < 0 ? player.position.y - mapCell.y : mapCell.y + 1 - player.position.y) * deltaStep.y
       );
-
-      let sideDistance = new Vector(firstStep.x, firstStep.y);
 
       let closestSideIsY = false;
       while (worldMap[mapCell.y][mapCell.x] == 0) {
-        if (sideDistance.x < sideDistance.y) {
-          sideDistance.x += deltaStep.x;
-          mapCell.x += Math.sign(rayDirection.x);
-          closestSideIsY = false;
-        } else {
+        closestSideIsY = sideDistance.y < sideDistance.x;
+        if (closestSideIsY) {
           sideDistance.y += deltaStep.y;
           mapCell.y += Math.sign(rayDirection.y);
-          closestSideIsY = true;
+        } else {
+          sideDistance.x += deltaStep.x;
+          mapCell.x += Math.sign(rayDirection.x);
         }
       }
-
       let distanceToWall = closestSideIsY ? sideDistance.y - deltaStep.y : sideDistance.x - deltaStep.x;
 
-      let lineHeight = Math.floor(innerHeight / distanceToWall);
-      let drawStart = (innerHeight - lineHeight) / 2;
-
-      let textureIndex = worldMap[mapCell.y][mapCell.x] - 1;
-
       let wallX = 0.0;
-      if (!closestSideIsY) wallX = position.y + distanceToWall * rayDirection.y;
-      else wallX = position.x + distanceToWall * rayDirection.x;
+      if (closestSideIsY) wallX = player.position.x + distanceToWall * rayDirection.x;
+      else wallX = player.position.y + distanceToWall * rayDirection.y;
       wallX -= Math.floor(wallX);
 
+      let texture = textures[worldMap[mapCell.y][mapCell.x] - 1];
       let textureX = Math.floor(wallX * textureSize);
-      ctx.drawImage(textures[textureIndex], textureX, 0, 1, textureSize, x, drawStart, 1, lineHeight);
+      let lineHeight = Math.floor(innerHeight / distanceToWall);
+      let drawStart = (innerHeight - lineHeight) / 2;
+      ctx.drawImage(texture, textureX, 0, 1, textureSize, x, drawStart, 1, lineHeight);
 
-      if (!closestSideIsY) {
-        ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
-        ctx.beginPath();
-        ctx.moveTo(x, drawStart);
-        ctx.lineTo(x, drawStart + lineHeight);
-        ctx.stroke();
-      }
+      if (!closestSideIsY) drawBakedLightingLine(x, drawStart, lineHeight);
     }
+  }
+
+  function drawText() {
+    ctx.fillStyle = "green";
+    ctx.fillText(`FPS: ${fps.toFixed(0)}`, 24, 48);
+  }
+
+  function drawBakedLightingLine(x: number, drawStart: number, lineHeight: number) {
+    ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.beginPath();
+    ctx.moveTo(x, drawStart);
+    ctx.lineTo(x, drawStart + lineHeight);
+    ctx.stroke();
   }
 </script>
 
